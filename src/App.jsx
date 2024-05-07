9578dfbe1619114333f0c34f047873353dedcfb7
dev_ram <ram@gisfy.co.in>
2024-05-07 19:38:15 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <>
+    <div className="text-5xl">App</div>
+    <p>Hii</p>
+    <button type="button" class="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Purple</button>
+
+    </>
+  )
+}
+
+export default App
